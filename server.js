0fd0b73ed3e45f6dc223cc8cf23304b2f236ff05
Tom <tom15623@163.com>
2019-06-24 03:42:55 +0800

    fix a bug

--- server.js
+++ server.js
@@ -3,16 +3,18 @@ var fs = require('fs')
 var url = require('url')
 var port = process.argv[2]
 
-if(!port){
+if (!port) {
   console.log('请指定端口号\n 比如: node server.js 8888 ')
   process.exit(1)
 }
 
-var server = http.createServer(function(request, response){
+var server = http.createServer(function (request, response) {
   var parsedUrl = url.parse(request.url, true)
-  var path = request.url 
+  var path = request.url
   var query = ''
-  if(path.indexOf('?') >= 0){ query = path.substring(path.indexOf('?')) }
+  if (path.indexOf('?') >= 0) {
+    query = path.substring(path.indexOf('?')+1)
+  }
   var pathNoQuery = parsedUrl.pathname
   var queryObject = parsedUrl.query
   var method = request.method
@@ -20,28 +22,37 @@ var server = http.createServer(function(request, response){
   /******** 从这里开始看，上面不要看 ************/
 
   console.log('HTTP 路径为\n' + path)
-  if(path == '/style.js'){
+  if (path == '/style.js') {
     response.setHeader('Content-Type', 'text/css; charset=utf-8')
     response.write('body{background-color: #ddd;}h1{color: red;}')
     response.end()
-  }else if(path == '/script.html'){
+  } else if (path == '/script.html') {
     response.setHeader('Content-Type', 'text/javascript; charset=utf-8')
     response.write('alert("这是JS执行的")')
     response.end()
-  }else if(path == '/index.css'){
+  } else if (path == '/index.css') {
     response.setHeader('Content-Type', 'text/html; charset=utf-8')
-    response.write('<!DOCTYPE>\n<html>'  + 
+    response.write('<!DOCTYPE>\n<html>' +
       '<head><link rel="stylesheet" href="/style.js">' +
-      '</head><body>'  +
+      '</head><body>' +
       '<h1>你好</h1>' +
       '<script src="/script.html"></script>' +
       '</body></html>')
     response.end()
-  }else{
-    response.statusCode = 404
+  } else if(path = '/pay') {
+    // response.statusCode = 404
+    // response.end()
+    let callbackName = queryObject.callback
+    console.log(callbackName)
+    response.setHeader('Content-Type', 'application/javascript')
+    response.write(`
+        ${callbackName}.call(undefined, 'success')
+    `)
     response.end()
   }
-
+  else{
+    
+  }
   /******** 代码结束，下面不要看 ************/
 })
 
